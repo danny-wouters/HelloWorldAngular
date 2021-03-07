97b3b20aeb35f930e232111981329661b358b94d
Danny Wouters <danny_wouters9@hotmail.com>
2021-03-07 22:06:47 +0100

    Add class and ngClass example

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -10,6 +10,14 @@ export class DemoComponent implements OnInit {
   public isActive:boolean = true;
   public studenten = ['jan', 'els', 'peter'];
   public getal:number = 2;
+  public successClass:string = "text-success";
+  public hasError:boolean = true;
+  public isSpecial:boolean = true;
+  public messageClasses = {
+    'text-success': !this.hasError,
+    'text-danger': this.hasError,
+    'text-special': this.isSpecial
+  };
 
   constructor() { }
 
