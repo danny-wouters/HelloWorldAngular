11de928b80b4fa5c028b0739e2aa598012bbd712
Danny Wouters <danny_wouters9@hotmail.com>
2021-03-14 17:45:10 +0100

    Add HomeComponent + add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { LoggerService } from './logger.service';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'demo', component: DemoComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [LoggerService],
   bootstrap: [AppComponent]
