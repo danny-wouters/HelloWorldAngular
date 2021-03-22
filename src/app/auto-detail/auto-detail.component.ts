da478c94e1f466362a8b7e0f93b56220f4f97e7f
Danny Wouters <danny_wouters9@hotmail.com>
2021-03-22 17:54:49 +0100

    Add paramMap Observable example

--- src/app/auto-detail/auto-detail.component.ts
+++ src/app/auto-detail/auto-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-auto-detail',
@@ -7,13 +7,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./auto-detail.component.css']
 })
 export class AutoDetailComponent implements OnInit {
-  public autoId;
+  public autoId: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.autoId = id;
+    //let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    //this.autoId = id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id: number = parseInt(params.get('id'));
+      this.autoId = id;
+    })
   }
 
+  goPrevious(): void {
+    let previousId = this.autoId - 1;
+    this.router.navigate(["autos", previousId]);
+  }
+
+  goNext(): void {
+    let previousId = this.autoId + 1;
+    this.router.navigate(["autos", previousId]);
+  }
 }
