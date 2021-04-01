10731b3a24194bccb9781c9eab73b60be2b86948
Danny Wouters <danny_wouters9@hotmail.com>
2021-04-01 17:22:15 +0200

    Map api object to Angular object in BestellingService

--- src/app/bestelling/_models/bestelling.ts
+++ src/app/bestelling/_models/bestelling.ts
@@ -1,5 +1,11 @@
 export class Bestelling {
-    bestellingID: number;
-    artikel: string;
-    prijs: number;
+    public bestellingId: number;
+    public artikel: string;
+    public prijs: number;
+
+    constructor( bestellingId: number, artikel: string, prijs: number ) {
+        this.bestellingId = bestellingId;
+        this.artikel = artikel;
+        this.prijs = prijs;
+    }
 }
--- src/app/bestelling/_services/bestelling.service.ts
+++ src/app/bestelling/_services/bestelling.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Bestelling } from '../_models/bestelling';
+import { map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,12 @@ export class BestellingService {
   constructor(private _http: HttpClient) { }
 
   getBestellingen(): Observable<Bestelling[]> {
-    return this._http.get<Bestelling[]>(environment.apiUrl + 'bestelling');
+    return this._http.get<Bestelling[]>(environment.apiUrl + 'bestelling')
+      .pipe(map((entries: any[]) =>
+        entries.map((entry: any) =>
+           new Bestelling(entry.bestellingID, entry.artikel, entry.prijs)
+        )
+      )
+    );
   }
 }
